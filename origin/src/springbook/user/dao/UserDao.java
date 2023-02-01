7f5fab5a723c59dba5d65d75ec730e4b26e94c02
danny <gryb809@gmail.com>
2023-02-01 14:52:34 +0900

    final

--- origin/src/springbook/user/dao/UserDao.java
+++ origin/src/springbook/user/dao/UserDao.java
@@ -12,37 +12,33 @@ import org.springframework.jdbc.core.RowMapper;
 import springbook.user.domain.User;
 
 public class UserDao {
-	private DataSource dataSource;
-		
 	public void setDataSource(DataSource dataSource) {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
-			
-		this.dataSource = dataSource;
 	}
 	
 	private JdbcTemplate jdbcTemplate;
+	
+	private RowMapper<User> userMapper = 
+		new RowMapper<User>() {
+				public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+				User user = new User();
+				user.setId(rs.getString("id"));
+				user.setName(rs.getString("name"));
+				user.setPassword(rs.getString("password"));
+				return user;
+			}
+		};
+
 	
 	public void add(final User user) {
 		this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)",
 						user.getId(), user.getName(), user.getPassword());
 	}
 
-
 	public User get(String id) {
 		return this.jdbcTemplate.queryForObject("select * from users where id = ?",
-				new Object[] {id}, 
-				new RowMapper<User>() {
-					public User mapRow(ResultSet rs, int rowNum)
-							throws SQLException {
-						User user = new User();
-						user.setId(rs.getString("id"));
-						user.setName(rs.getString("name"));
-						user.setPassword(rs.getString("password"));
-						return user;
-					}
-				});
+				new Object[] {id}, this.userMapper);
 	} 
-
 
 	public void deleteAll() {
 		this.jdbcTemplate.update("delete from users");
@@ -53,17 +49,7 @@ public class UserDao {
 	}
 
 	public List<User> getAll() {
-		return this.jdbcTemplate.query("select * from users order by id",
-				new RowMapper<User>() {
-					public User mapRow(ResultSet rs, int rowNum)
-							throws SQLException {
-						User user = new User();
-						user.setId(rs.getString("id"));
-						user.setName(rs.getString("name"));
-						user.setPassword(rs.getString("password"));
-						return user;
-					}
-				});
+		return this.jdbcTemplate.query("select * from users order by id",this.userMapper);
 	}
 
 }
