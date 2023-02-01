60f08b42211c063334ae1a1b8e675ca4b04a7439
danny <gryb809@gmail.com>
2023-02-01 14:26:01 +0900

    2.3.5

--- origin/src/springbook/user/dao/UserDao.java
+++ origin/src/springbook/user/dao/UserDao.java
@@ -7,11 +7,13 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+import org.springframework.dao.EmptyResultDataAccessException;
+
 import springbook.user.domain.User;
 
 public class UserDao {
 	private DataSource dataSource;
-	
+		
 	public void setDataSource(DataSource dataSource) {
 		this.dataSource = dataSource;
 	}
@@ -36,17 +38,22 @@ public class UserDao {
 		PreparedStatement ps = c
 				.prepareStatement("select * from users where id = ?");
 		ps.setString(1, id);
-
+		
 		ResultSet rs = ps.executeQuery();
-		rs.next();
-		User user = new User();
-		user.setId(rs.getString("id"));
-		user.setName(rs.getString("name"));
-		user.setPassword(rs.getString("password"));
+
+		User user = null;
+		if (rs.next()) {
+			user = new User();
+			user.setId(rs.getString("id"));
+			user.setName(rs.getString("name"));
+			user.setPassword(rs.getString("password"));
+		}
 
 		rs.close();
 		ps.close();
 		c.close();
+		
+		if (user == null) throw new EmptyResultDataAccessException(1);
 
 		return user;
 	}
